497598a3e84bbf58c012c0e79dd943bb377c8526
mickor1996 <458349623@qq.com>
2020-03-12 14:27:39 +0800

    new

---
+++ 剑指/字符流中第一个不重复的字符.js
@@ -0,0 +1,41 @@
+// 题目描述
+// 请实现一个函数用来找出字符流中第一个只出现一次的字符。例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+// 输出描述:
+// 如果当前字符流没有存在出现一次的字符，返回#字符。
+//Init module if you need
+function Init() {
+  // write code here
+
+}
+const map = new Map()
+//Insert one char from stringstream
+function Insert(ch) {
+  // write code here
+  if (map.get(ch) == undefined) {
+    map.set(ch, 1)
+  } else {
+    map.set(ch, map.get(ch) + 1)
+  }
+}
+//return the first appearence once char in current stringstream
+function FirstAppearingOnce() {
+  // write code here
+  for (let k of map) {
+    if (k[1] == 1) {
+      return k[0]
+    }
+  }
+  return '#'
+}
+Insert('g')
+console.log(FirstAppearingOnce())
+Insert('o')
+console.log(FirstAppearingOnce())
+Insert('o')
+console.log(FirstAppearingOnce())
+Insert('g')
+console.log(FirstAppearingOnce())
+Insert('l')
+console.log(FirstAppearingOnce())
+Insert('e')
+console.log(FirstAppearingOnce())
