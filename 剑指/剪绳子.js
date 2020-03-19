2148de03613419bcfaafcbda937a1ae661fb3824
mickor1996 <458349623@qq.com>
2020-03-19 12:44:12 +0800

    last

---
+++ 剑指/剪绳子.js
@@ -0,0 +1,28 @@
+// 题目描述
+// 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1），每段绳子的长度记为k[0],k[1],...,k[m]。请问k[0]xk[1]x...xk[m]可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+// 输入描述:
+// 输入一个数n，意义见题面。（2 <= n <= 60）
+// 输出描述:
+// 输出答案。
+// 示例1
+// 输入
+// 8
+// 输出
+// 18
+function cutRope(number) {
+  if (number <= 4) return number
+  const map = new Map()
+  map.set(1, 1)
+  for (let i = 2; i <= number; i++) {
+    let max = i
+    for (let j = 1; j <= i / 2; j++) {
+      let temp = map.get(j) * map.get(i - j)
+      if (temp > max) {
+        max = temp
+      }
+    }
+    map.set(i, max)
+  }
+  return map.get(number)
+}
+console.log(cutRope(10))
--- 剑指/机器人的运动范围.js
+++ 剑指/机器人的运动范围.js
@@ -1,7 +1,7 @@
 // 题目描述
 // 地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
 function movingCount(threshold, rows, cols) {
-    if (rows == 0 || cols == 0) {
+    if (rows == 0 || cols == 0 || threshold < 0) {
         return 0
     }
     let visit = []
@@ -60,4 +60,4 @@ function isSatisfied(threshold, temp) {
     return false
 }
 console.log(isSatisfied(15, [9, 19]))
-console.log(movingCount(15, 20, 20))
+console.log(movingCount(-10, 20, 20))
